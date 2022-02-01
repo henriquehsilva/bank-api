60e139984aaf10ba9b70b7644dae3ac32d61a7ea
Henrique Silva <henrique@henriquesilva.dev>
2022-02-01 10:40:53 -0300

    create model for account and customer

---
+++ db/migrate/20220131222303_create_customers.rb
@@ -0,0 +1,11 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :cpf
+      t.references :account, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
